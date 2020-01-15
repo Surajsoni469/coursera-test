b7b744ea399cbbda7232221599fce8df7e28d237
Surajsoni469 <58718186+Surajsoni469@users.noreply.github.com>
2020-01-15 10:59:33 +0530

    Module-4 Easier

--- mod4-solution(easier)/js/Goodbye.js
+++
@@ -1,7 +0,0 @@
-var byeSpeaker = {};
-byeSpeaker.name = ["John", "Jen", "Jason", "Jim"];
-var speakWord = "Good Bye";
-
-byeSpeaker.names = function speak(names) {
-  console.log(speakWord + " " + byeSpeaker.names);
-}
